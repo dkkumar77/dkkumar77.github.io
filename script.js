52b39b7d29c2187113245892b3ec9fe623e61f73
Deepak Kumar <61252338+dkkumar77@users.noreply.github.com>
2024-05-30 14:01:19 -0400

    typing effect

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var typed = new Typed('#typed-output', {
+        strings: ["Computer Science graduate with a keen interest in network security, database management, and cloud computing. Proficient in AWS for deploying applications and implementing security measures. Seeking career opportunities to leverage my skills in a dynamic work environment etc. etc. etc."],
+        typeSpeed: 05,
+        backSpeed: 20,
+        loop: true,
+    });
+});
