1c76a298a840b53aedc30c4061db2f672be5daeb
Deepak Kumar <61252338+dkkumar77@users.noreply.github.com>
2024-05-30 23:16:46 -0400

    wroked on blog 2

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     var typed = new Typed('#typed-output', {
-        strings: ["Computer Science graduate with a keen interest in network security, database management, and cloud computing. Proficient in AWS for deploying applications and implementing security measures. Seeking career opportunities to leverage my skills in a dynamic work environment etc. etc. etc."],
-        typeSpeed: 05,
-        backSpeed: 20,
+        strings: [
+            "Computer Science graduate with a keen interest in network security, database management, and cloud computing. Proficient in AWS for deploying applications and implementing security measures. Seeking career opportunities to leverage my skills in a dynamic work environment etc. etc. etc.",
+        ],
+        typeSpeed: 5, // Removed leading zero
+        backSpeed: 60,
+        backDelay: 2000,
         loop: true,
     });
 });
