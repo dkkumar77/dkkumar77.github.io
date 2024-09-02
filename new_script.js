11a691af98963e22c2990cc3a97c84339560a2d7
Deepak Kumar <61252338+dkkumar77@users.noreply.github.com>
2024-09-02 00:11:27 -0400

    updated

---
+++ new_script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var typed = new Typed('#typed-output', {
+        strings: [
+
+         
+
+
+            "Networking.   <br> Software. <br> Cloud. ",
+        ],
+        typeSpeed: 5, 
+        backSpeed: 60,
+        backDelay: 2000,
+        loop: false,
+    });
+});
